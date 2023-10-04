35c6a17b36e12fb82ef38e3d819b9b76d71a0531
joams mogro gomez <joamsmg@gmail.com>
2023-10-04 00:14:04 -0400

    adding router outlet to home page

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
