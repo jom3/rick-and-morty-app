15fb0f33102dcf58be49daac5edb21e21d6a3617
joams mogro gomez <joamsmg@gmail.com>
2023-10-04 00:16:02 -0400

    adding change detection

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterOutlet],
+  changeDetection:ChangeDetectionStrategy.OnPush,
+  imports: [
+    RouterOutlet,
+    NavbarComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
