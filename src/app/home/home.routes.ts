0bdd1162022277d5f68c658a461b196c9dfe9d07
joams mogro gomez <joamsmg@gmail.com>
2023-10-04 16:41:02 -0400

    adding characters, episodes, locations routes

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'', loadChildren:()=>import('../characters/characters.routes').then(r=>r.routes)
+  },
+  {
+    path:'', loadChildren:()=>import('../episodes/episodes.routes').then(r=>r.routes)
+  },
+  {
+    path:'', loadChildren:()=>import('../locations/locations.routes').then(r=>r.routes)
+  },
+];
