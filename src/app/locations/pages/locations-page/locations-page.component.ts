6e673a7bb963b4c539769daee7a55ee7aed93f0a
joams mogro gomez <joamsmg@gmail.com>
2023-10-08 18:37:05 -0400

    adding data to locations page

--- src/app/locations/pages/locations-page/locations-page.component.ts
+++ src/app/locations/pages/locations-page/locations-page.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Info, Location } from '../../interfaces/locations-response.interface';
+import { LocationsService } from '../../services/locations.service';
+import { LocationsListComponent } from '../../components/locations-list/locations-list.component';
 
 @Component({
   selector: 'app-locations-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LocationsListComponent],
   templateUrl: './locations-page.component.html',
   styleUrls: ['./locations-page.component.css']
 })
-export class LocationsPageComponent {
+export class LocationsPageComponent implements OnInit{
+
+  public locations = signal<Location[]>([])
+  public currentPage = signal<number>(1)
+  public pageData = signal<Info>({count: 0,next:'',pages:0,prev:''})
+
+  private locationService = inject(LocationsService)
+
+  ngOnInit(): void {
+    this.getAllLocations()
+  }
+
+  getAllLocations(){
+    this.locationService.getAllLocations(this.currentPage()).subscribe({
+      next:r=>{
+        console.log(r.results)
+        this.pageData.set(r.info)
+        this.locations.set(r.results)
+      },
+      error:e=>{
+        console.log(e) //* #FIXME: agregar cuando haya un componente para el error
+      },
+    })
+  }
+
+  changeCurrentPage(value:number){
+    this.currentPage.set(this.currentPage()+value)
+    this.getAllLocations()
+  }
 
 }
