3aed2b0517b1f5e8e0afc581a968581822b88729
joams mogro gomez <joamsmg@gmail.com>
2023-10-08 18:36:14 -0400

    adding interface for locations

---
+++ src/app/locations/interfaces/locations-response.interface.ts
@@ -0,0 +1,21 @@
+export interface LocationsResponse {
+  info:    Info;
+  results: Location[];
+}
+
+export interface Info {
+  count: number;
+  pages: number;
+  next:  string;
+  prev:  string;
+}
+
+export interface Location {
+  id:        number;
+  name:      string;
+  type:      string;
+  dimension: string;
+  residents: string[];
+  url:       string;
+  created:   Date;
+}
